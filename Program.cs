d60d39277db1c65605de299c7d38cb4f9cd8d5b3
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-21 07:06:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //if condition
+            if (condition)
+            {
+                someStatement
+            }
+
+            else if (anotherCondition)
+            {
+                anotherStatement
+            }
+            else
+            {
+                yetAnotherStatement
+            }
+            //nested if statements - avoid multiple nested if statements
+            //since it's difficult to deal with that code
+            if (condition)
+            {
+                if (anotherCondition)
+                {
+                    ...
+                    else
+                    {
+
+                    }
+                    ...
+
+                }
+            }
+            //switch condition
+            // role is a variable that is compared with different values
+            switch (role)
+            {
+                case Role.Admin: // a value
+                    ...
+                    break;
+                case Role.Moderator: // a value
+                    ...
+                    break;
+                default:    // a value
+                    ...
+                    break;
+            }
+            */
+        }
+    }
+}
